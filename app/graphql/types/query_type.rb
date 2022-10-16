b8c1694169bb72041c57e8043d394a0b55ba8b73
John Santosuosso <john.santosuosso1@gmail.com>
2022-10-16 16:26:43 -0400

    add basic user functionality

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :users,
+    [Types::UserType]
+    null: false,
+    description: "Return all users"
+    def users
+      User.all
+    end
+  end
+end
